4500539fd5337c091b4535d6b105f39658879c0e
Changhsiaotiu <love1992810707@gmail.com>
2019-11-29 15:47:44 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-hmcxpi-qtkuhf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-hmcxpi-qtkuhf)
---
+++ src/app/leftmainmenu.ts
@@ -0,0 +1,25 @@
+export const leftmainmenu = [
+  {
+    name: '首頁',
+    description: '點擊進入首頁'
+  },
+  {
+    name: '整批開戶處理',
+    description: '點擊開戶查詢'
+  },
+  {
+    name: '處理中案件查詢',
+    description: ''
+  },
+  {
+    name: '整批開戶查詢',
+    description: ''
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
